14c693c557bb257d69cc72d3ed932a0729d5ccd9
hangnznguyen <hangnznguyen@gmail.com>
2025-02-14 19:50:06 +1300

    adding more exericse

---
+++ Module1/Backend/Helloworld.js
@@ -0,0 +1,43 @@
+
+function Showmessages(name)
+{
+    console.log("hello" + name )
+    
+}
+
+Showmessages("Graham")
+Showmessages("Rose")
+Showmessages("Gavin")
+
+function sum(a,b)
+{
+    return a + b
+}
+
+sum(12,2)
+sum(10,5)
+sum(6,8)
+
+
+console.log("Hello Mision")
+
+function sum(a, b)
+{
+    return a + b
+}
+function abstract(a, b)
+{
+    return a - b
+}
+function multiply(a, b)
+{
+    return a * b
+}
+function divide(a, b)
+{
+    return a/b
+}
+console.log(sum(10, 3))
+console.log(abstract(4, 3))
+console.log(divide(10, 5))
+console.log(multiply(2, 3))
---
+++ Module1/Labexercise/Lab4.js
@@ -0,0 +1,30 @@
+function sum(a, b)
+{
+    return a + b
+}
+function abstract(a, b)
+{
+    return a - b
+}
+function multiply(a, b)
+{
+    return a * b
+}
+function divide(a, b)
+{
+    return a/b
+}
+console.log(sum(10, 3))
+console.log(abstract(4, 3))
+console.log(divide(10, 5))
+console.log(multiply(2, 3))
+
+function Showmessages(name)
+{
+    console.log("Hello" + name)
+}
+
+
+Showmessages("Tony")
+Showmessages("John")
+Showmessages("Hoa")
